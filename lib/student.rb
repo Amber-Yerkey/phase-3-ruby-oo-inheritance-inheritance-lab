b1ac35a2c29352b59fc632ef2eecef5719defa79
Amber Frantz <amber.k.frantz@mgail.com>
2022-07-23 17:52:25 -0400

    Completed Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative './user'
 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(study)
+        @knowledge << study
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
